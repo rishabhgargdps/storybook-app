667c6608d2a83cc564811de23160e5877d3f0984
rishabhgargdps <rishabhgargbits@gmail.com>
2020-08-19 23:24:22 +0530

    Dashboard page

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 const exphbs = require("express-handlebars");
 const morgan = require("morgan");
 const passport = require("passport");
-const session = require("express-session")
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
 
 //Passport config
-require("./config/passport")(passport)
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -26,22 +28,25 @@ app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 //Sessions
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //routes
 app.use("/", require("./routes/index"));
-app.use('/auth', require('./routes/auth'))
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get("/", (req, res) => {
+const Story = require("../models/Story");
+
+router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
     layout: "login",
   });
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("../views/error/404.hbs");
+  }
 });
 
 module.exports = router;
