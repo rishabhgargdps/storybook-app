8e6b7d64f7126de61c9e0d0f3c9702d1aa1846cd
rishabhgargdps <rishabhgargbits@gmail.com>
2020-08-19 11:46:53 +0530

    OAuth functionality and templates rendered

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db");
+const exphbs = require("express-handlebars");
 const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session")
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
+
+//Passport config
+require("./config/passport")(passport)
 
 connectDB();
 
@@ -13,6 +20,28 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//Handlebars middleware
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+//Sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//routes
+app.use("/", require("./routes/index"));
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000;
 
